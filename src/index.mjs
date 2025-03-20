b90b2e71688610cd957e1318467f30217cfbdc8d
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2025-03-20 11:41:16 +0000

    11

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,39 @@
-import "./styles.css";
+//higher order function
+function x() {
+  console.log("aaaaaa");
+}
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+function y(x) {
+x ();
+
+// } 
+
+// x is callback and y is higher order function 
+
+
+
+const radius = [ 3,1,2,4];
+
+const calcuArea = function (radius){
+
+    const output = [];
+    for (let i = 0 ; i < radius.length; i++){
+        output.push(Math.PI* radius[i] * radius[i] )
+    }
+    return output;
+}
+console.log(calcuArea(radius));
+
+
+
+// for circumference
+
+const calcuCircum ( radius){
+const output = [];
+
+for ( let i = 0 ; i < radius.length ; i++)
+    {
+        output.push(2*PI*radius[i])
+    }
+return output ;
+};
